04b2b99e68a8f49f7db3f6d36e5f2afbe3a85d77
Andre Silva <silva603@gmail.com>
2022-02-15 22:08:10 -0300

    Create sub_domains.py

---
+++ sub_domains.py
@@ -0,0 +1,18 @@
+#code from THM Python4Pentesters
+import requests
+import sys
+
+sub_list = open("wl2.txt").read()
+subdoms = sub_list.splitlines()
+
+for sub in subdoms:
+    sub_domains = f"http://{sub}.{sys.argv[1]}"
+
+    try:
+        requests.get(sub_domains)
+
+    except requests.ConnectionError:
+        pass
+
+    else:
+        print("Valid domain: ",sub_domains
